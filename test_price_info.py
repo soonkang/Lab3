d8aa29a43de2a76024c60fbc24f2a02b2f062c93
soonkang <soonkang.22@ichat.sp.edu.sg>
2023-05-05 14:47:32 +0800

    new

--- test_price_info.py
+++ test_price_info.py
@@ -1,8 +1,8 @@
+import price_info
 
 price_list={'apple' : 1.20, 'orange':1.40, 'watermelon': 6.50, 'pineapple': 2.70, 'pear' : 0.90, 'papaya': 2.95, 'pomegranate': 4.95 }
 
 quantity_list= {'apple': 5, 'orange':5, 'watermelon': 1, 'pineapple': 2, 'pear' : 10, 'papaya': 1, 'pomegranate': 2}
-
 
 def total_cost_shopping():
     total_cost = 0
@@ -11,8 +11,6 @@ def total_cost_shopping():
             # complete the implementation below:
             total_cost += price_list[key] * quantity_list[key]
             print("total cost = ", total_cost)
-
-
 def cost_of_fruits(fruit, quantity):
     for key in price_list.keys():
         if key == fruit:
@@ -21,12 +19,10 @@ def cost_of_fruits(fruit, quantity):
 
     print("cost of ", quantity, fruit, "=", cost)
 
-
 def main():
 
-    cost_of_fruits('apple', 10)
+    cost_of_fruits('orange', 10)
     total_cost_shopping()
-
 
 if __name__ == "__main__":
     main()
