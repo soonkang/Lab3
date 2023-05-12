fbcac4b7a2588f93cee93763b3317ff186628a12
soonkang <soonkang.22@ichat.sp.edu.sg>
2023-05-12 17:44:48 +0800

    edited

--- test_price_info.py
+++ test_price_info.py
@@ -1,10 +1,17 @@
-import pytest
-from price_info import total_cost_shopping, cost_of_fruits
+from price_info import *
+
 
 def test_total_cost_shopping():
-    assert total_cost_shopping() == 31.75
+    result = total_cost_shopping()
+    assert result == 46.75
 
 def test_cost_of_fruits():
     assert cost_of_fruits('apple', 5) == 6.0
     assert cost_of_fruits('watermelon', 1) == 6.5
     assert cost_of_fruits('pear', 10) == 9.0
+
+
+if __name__ == '__main__':
+    test_total_cost_shopping()
+    test_cost_of_fruits()
+    print('All tests passed')
